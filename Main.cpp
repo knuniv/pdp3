ae1d3e34ac9663cc207666149bc9d4c68b988c8e
yura <stellerator2@i.ua>
2009-09-28 23:03:19 +0300

    axis j weighting

--- Main.cpp
+++ Main.cpp
@@ -13,15 +13,16 @@ int main()
 	///dcommebvbvbvbvbvb
 	PML pml1(0.0,0.25, 0.001,1);
 	Geometry geom1(128,128, 129, 129, &pml1);
-	Time time1(0,0, 1,1);
+	Time time1(0,0, 20000,1);
 //	Particles particle1(1000, &geom1);
 	E_field e_field1(&geom1);
 	H_field h_field1(&geom1);
 	Fourier four1(0);
+	bool res;
 	//double** ft;
 	current current1(&geom1);
-	charge_density ro1(&geom1);
-	charge_density ro2(&geom1);
+	charge_density rho_new(&geom1);
+	charge_density rho_old(&geom1);
 	//ft = new double* [1];
 	//for (int i=0; i<1;i++)
 	//{
@@ -40,16 +41,46 @@ int main()
 	geom1.set_epsilon();
 //	e_field1.set_sigma();
 
-	Particles particle1("ions", 1, 1, 1, &geom1);
-	particle1.j_weighting(&time1,&current1,2.6,2.5,2.0001,2.0001);
-	particle1.x1[0] = 2.0001;
-    particle1.x3[0] = 2.0001;
-	particle1.charge_weighting(&ro1);
-    particle1.x1[0] = 2.6;
-    particle1.x3[0] = 2.5;
-	particle1.charge_weighting(&ro2);
+	Particles new_particles("ions", 1, 1, 1, &geom1), old_particles("ions", 1, 1, 1, &geom1);
+	//////////////////////
+	double new_x1 =0.900000000001;
+	double new_x3 = 1.3001;
+	double old_x1 = 0.6;
+	double old_x3 = 1.3;
+    new_particles.x1[0]=old_x1;
+	new_particles.x3[0]=old_x3;
+	new_particles.charge_weighting(&rho_old);
+	new_particles.x1[0]=new_x1;
+	new_particles.x3[0]=new_x3;
+	new_particles.charge_weighting(&rho_new);
+	new_particles.j_weighting(&time1,&current1,new_x1,new_x3,old_x1,old_x3);
+    res =  continuity_equation(&time1, &geom1, &current1, &rho_old, &rho_new);
 
-    double res =  continuity_equation(&time1, &geom1, &current1, &ro1, &ro2, 2,2);
+	//////////////////////////////////
+
+	new_particles.j_weighting(&time1,&current1,2.6,2.5,2.0001,2.0001);
+	new_particles.x1[0] = 127.0001;
+    new_particles.x3[0] = 127.0001;
+	new_particles.v1[0] = 0.7;
+	new_particles.v3[0] = 0.6;
+	new_particles.charge_weighting(&rho_new);
+	res =  continuity_equation(&time1, &geom1, &current1, &rho_old, &rho_new);
+
+    while (time1.current_time < time1.end_time)
+	{
+		new_particles.charge_weighting(&rho_old);
+        old_particles.x1[0] = new_particles.x1[0];
+		old_particles.x3[0] = new_particles.x3[0];
+		new_particles.half_step_coord(&time1);
+		new_particles.charge_weighting(&rho_new);
+		new_particles.j_weighting(&time1,&current1, new_particles.x1[0], new_particles.x3[0], 
+			                                        old_particles.x1[0], old_particles.x3[0]);
+		res =  continuity_equation(&time1, &geom1, &current1, &rho_old, &rho_new); 
+		time1.current_time = time1.current_time + time1.delta_t;
+		if (!res)
+			break;
+	}
+	
 
 	//particle1.velocity_distribution(1E5);
 
--- Particles.cpp
+++ Particles.cpp
@@ -3,6 +3,7 @@
 #include "H_field.h"
 #include "Time.h"
 #include "Triple.h"
+using namespace std;
 //#include "stdafx.h"
 
 Particles::Particles(void)
@@ -140,11 +141,44 @@ void Particles::step_v(E_field* e_fld, H_field* h_fld, Time* t)
 void Particles::half_step_coord(Time* t)
 {
 	int i;
+	double dr = geom1->dr;
+	double dz = geom1->dz;
+	double x1_wall = geom1->first_size - dr/2.0;
+	double x3_wall = geom1->second_size - dz/2.0;
+	double half_dr = dr/2.0;
+	double half_dz = dz/2.0;
+	double x1_wallX2 = x1_wall*2.0;
+	double x3_wallX2 = x3_wall*2.0;
+
 	for( i=0;i<number;i++)
 		if (is_alive[i])
 		{	
 			x1[i] = x1[i] + v1[i]*t->delta_t/2.0;
             x3[i] = x3[i] + v3[i]*t->delta_t/2.0;
+
+			if (x1[i] > x1_wall)
+			{
+				x1[i] = x1_wallX2 - x1[i];
+				v1[i] = -v1[i];
+			}
+
+			if (x3[i] > x3_wall)
+			{
+				x3[i] = x3_wallX2 - x3[i];
+				v3[i] = -v3[i];
+			}
+
+			if (x1[i] < half_dr)
+			{
+				x1[i] = - x1[i];
+				v1[i] = -v1[i];
+			}
+
+			if (x3[i] < half_dz)
+			{
+				x3[i] = - x3[i];
+				v3[i] = -v3[i];
+			}
 		}
 }
 
@@ -159,7 +193,7 @@ void Particles::charge_weighting(charge_density* ro1)
 	int r_i=0;  // number of particle i cell 
 	int z_k=0;  // number of particle k cell
 	
-	double dr = geom1->dr;
+	double dr = geom1->dr; 
 	double dz = geom1->dz;
 	double r1, r2, r3; // temp variables for calculation
 	double dz1, dz2;   // temp var.: width of k and k+1 cell 
@@ -170,9 +204,15 @@ void Particles::charge_weighting(charge_density* ro1)
 	//double ro_v_2=0; // charge density in i+1 cell
 	
 	double value =0;
+	double **temp = ro1->get_ro();
+	int i,k;
 
-
-	for(int i=0;i<number;i++)
+	for (i=0;i<geom1->n_grid_1;i++)
+		for (k=0;k<geom1->n_grid_2;k++)
+			//ro1->set_ro_weighting(i,k,-temp[i][k]);
+			temp[i][k] = 0.0;
+			
+	for(i=0;i<number;i++)
 	{
             // finding number of i and k cell. example: dr = 0.5; r = 0.4; i =0
 		////////////////////////////
@@ -219,6 +259,7 @@ void Particles::charge_weighting(charge_density* ro1)
 			 r3 = x1[i]+0.5*dr;
 			 dz1 = (z_k+1)*dz-x3[i];
 			 dz2 = x3[i] - z_k*dz;
+			 ro_v = charge/(2.0*pi*dz*dr*x1[i]);
 			 v_1 = pi*dz*dr*dr/4.0;
 			 v_2 = pi*dz*dr*dr*2.0*(r_i+1);
 		   ///////////////////////////
@@ -366,6 +407,8 @@ void Particles::simple_j_weighting(Time* time1, current *j1, double x1_new,doubl
 		// distance of particle moving//
 		double delta_r = x1_new - x1_old;
 		double delta_z = x3_new - x3_old;
+
+		// if i cell is not equal 0 
 		if (i_n>=1)
 		{
 		///////////////////////////////////
@@ -401,6 +444,7 @@ void Particles::simple_j_weighting(Time* time1, current *j1, double x1_new,doubl
 		 wj = charge/(2*pi*r0*dz*dz*dr*delta_t) * (-r0*k*delta_r - k/2.0*delta_r*(x1_old+delta_r/2.0)+0.5*delta_r*(b+k*(2*r0+r1))+ delta_r*(b+k*r1)*(4*r0*r0-dr*dr)/(8*x1_old*(x1_old+delta_r)) - (k*(r0*r0/2.0-dr*dr/8.0))*log((x1_old+delta_r)/x1_old));
 		j1->set_j1(i_n, k_n+1, wj);
 		}
+		// if i cell is equal 0 
 		else
 		{
 ///////////////////////////////////
@@ -409,12 +453,12 @@ void Particles::simple_j_weighting(Time* time1, current *j1, double x1_new,doubl
 		double k = delta_r/delta_z;
 		double b = x1_old;
 	    //calculate current jz in [i,k] cell//
-		wj = charge/(2*dr*dz*delta_t*pi*dr*dr/4.0) * (dr*delta_z - k*delta_z*delta_z/2.0 - delta_z*b + dr*dr/k * ((i_n+0.5)*(i_n+0.5)-0.25)*log((k*delta_z+b)/b)); 
+		wj = charge/(2.0*dr*dz*delta_t*pi*dr*dr/4.0) * (dr*delta_z - k*delta_z*delta_z/2.0 - delta_z*b ); 
 		// set new weighting current value 
 		j1->set_j3(i_n,k_n, wj);
 
         //calculate current in [i+1,k] cell//
-		wj = charge/(2*dr*dz*delta_t*2*pi*(i_n+1)*dr*dr) * (k*delta_z*delta_z/2.0+delta_z*b + delta_z*dr + dr*dr/k * (0.25-(i_n+0.5)*(i_n+0.5)) * log((k*delta_z+b)/b)); 
+		wj = charge/(2.0*dr*dz*delta_t*2.0*pi*dr*dr) * (k*delta_z*delta_z/2.0 + delta_z*dr +delta_z*b); 
 		// set new weighting current value 
 		j1->set_j3(i_n+1,k_n, wj);
 		
@@ -446,6 +490,34 @@ void Particles::j_weighting(Time* time1, current *j1, double x1_new,double x3_ne
 
 	double dr = geom1->dr;
 	double dz = geom1->dz;
+
+	double **J1 = j1->get_j1();
+	double **J2 = j1->get_j2();
+	double **J3 = j1->get_j3();
+	int i,k;
+
+	//////////////////////////////////////
+	   //initialization//
+
+	for (i=0; i<(geom1->n_grid_1-1);i++)
+		for (k=0; k<(geom1->n_grid_2-1);k++)
+		{
+			J1[i][k]=0;
+			J2[i][k]=0;
+			J3[i][k]=0;
+		}
+	 for (i=0; i<(geom1->n_grid_1-1);i++)
+		J1[i][geom1->n_grid_2-1]=0;
+
+	 for(k=0;k<(geom1->n_grid_2);k++)
+		J2[geom1->n_grid_1-1][k]=0;
+
+	 for(i=0;i<(geom1->n_grid_1);i++)
+		J2[i][geom1->n_grid_2-1]=0;
+
+	 for(k=0;k<(geom1->n_grid_2-1);k++)
+		J3[geom1->n_grid_1-1][k]=0;
+
 //defining number of cell
 	int i_n = (int)ceil((x1_new)/dr)-1;
 	int k_n =(int)ceil((x3_new)/dr)-1;
@@ -627,16 +699,30 @@ void Particles::j_weighting(Time* time1, current *j1, double x1_new,double x3_ne
 
 
 }
-double continuity_equation(Time *input_time, Geometry *input_geometry, current *input_J, charge_density *rho_start, charge_density *rho_end, int i, int k)
+bool continuity_equation(Time *input_time, Geometry *input_geometry, current *input_J, charge_density *rho_old, charge_density *rho_new)
 {
-	double **rho_start_array = rho_start->get_ro() ;
-    double **rho_end_array = rho_end->get_ro() ;
+	double **rho_old_array = rho_old->get_ro() ;
+    double **rho_new_array = rho_new->get_ro() ;
 	double **J1 = input_J->get_j1() ;
 	double **J3 = input_J->get_j3() ;
 	double delta_rho = 1.0/(input_geometry->dz*4.0*3.1415*input_geometry->dr*input_geometry->dr) ;
-	double  res = (rho_end_array[i][k] - rho_start_array[i][k]) +(J3[i][k] - J3[i][k-1])/input_geometry->dz   +(J1[i][k] - J1[i-1][k])/input_geometry->dr + (J1[i][k] + J1[i-1][k])/(2.0*i*input_geometry->dr);
+	int i, k;
+	bool ok = true;
+	double res, tolerance = 1e-10 ;
+	for (i=1;i<input_geometry->n_grid_1-1;i++)
 
-	return res;
+		for (k=1;k<input_geometry->n_grid_2-1;k++)
+		{
+			res = (rho_new_array[i][k] - rho_old_array[i][k]) +(J3[i][k] - J3[i][k-1])/input_geometry->dz   +(J1[i][k] - J1[i-1][k])/input_geometry->dr + (J1[i][k] + J1[i-1][k])/(2.0*i*input_geometry->dr);
+			if (res > tolerance)
+			{
+				ok = false;
+				std::cout<<i<<" "<<k;
+			    i = input_geometry->n_grid_1;
+				break;
+			}
+		}
+	return ok;
 	/*return delta_rho -(J3[i][k] - J3[i][k-1])/input_geometry->dz  +
 		(J1[i][k] + J1[i-1][k])/input_geometry->dr + (J1[i][k] + J1[i-1][k])/(i+0.5)/input_geometry->dr ;*/
 }
