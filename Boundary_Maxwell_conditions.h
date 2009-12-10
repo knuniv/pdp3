ba17d65f2a4dc590d4a3ffbf30af0b50b5260471
yura <yura.tolochkevich@gmail.com>
2009-12-10 18:40:31 +0200

    add Maxwell boundary conditions

---
+++ Boundary_Maxwell_conditions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "E_field.h"
+class Boundary_Maxwell_conditions
+{
+public:
+E_field* e_fld;
+
+public:
+	Boundary_Maxwell_conditions(void );
+	Boundary_Maxwell_conditions(E_field* e_fld );
+	~Boundary_Maxwell_conditions(void);
+	void specify_initial_field(Geometry* cyl_geom,double E_fi_upper, double E_fi_left, double E_fi_right);
+	void radiation_source(Geometry* cyl_geom, double region_size, double frequency, int type, double time);
+};
